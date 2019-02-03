1f6e24a813cef37dfbcbd0fc13fec275c98751ad
bitbybyte <bittobaito@gmail.com>
2019-02-02 23:40:32 -0500

    Download images, files to post directories

--- README.md
+++ README.md
@@ -8,8 +8,6 @@ Download media and other data from Fantia. A session key value must be provided 
  - requests
 
 ## Roadmap
- - Download images from posts
- - Download videos from posts
  - Download text, tags, and other metadata from posts
  - Build proper CLI
  - Custom filename and directory name templating
--- fantiadl.py
+++ fantiadl.py
@@ -4,19 +4,25 @@
 
 import requests
 
+from urllib.parse import urljoin
 import json
+import mimetypes
+import os
 import re
 import sys
+
 
 class FantiaDownloader:
     ME_API = "https://fantia.jp/api/v1/me"
     FANCLUB_API = "https://fantia.jp/api/v1/fanclubs/{}"
     FANCLUB_HTML = "https://fantia.jp/fanclubs/{}/posts?page={}"
     POST_API = "https://fantia.jp/api/v1/posts/{}"
+    POSTS_URL = "https://fantia.jp/posts"
     POST_URL_RE = re.compile(r"href=['\"]\/posts\/([0-9]+)")
 
-    def __init__(self, session_id):
+    def __init__(self, session_id, chunk_size=1024):
         self.session_id = session_id
+        self.chunk_size = chunk_size
         self.session = requests.session()
         self.login()
 
@@ -51,16 +57,64 @@ class FantiaDownloader:
                 page_number += 1
 
 
+    def perform_download(self, url, filename):
+        request = self.session.get(url, stream=True)
+        request.raise_for_status()
+        with open(filename, "wb") as file:
+            for chunk in request.iter_content(self.chunk_size):
+                file.write(chunk)
+
+
+    def download_photo(self, photo, photo_counter, gallery_directory):
+        download_url = photo["url"]["original"]
+        photo_header = self.session.head(download_url)
+        extension = mimetypes.guess_extension(photo_header.headers["Content-Type"], strict=True)
+        filename = os.path.join(gallery_directory, str(photo_counter) + extension) if gallery_directory else str()
+        self.perform_download(download_url, filename)
+
+
+    def download_video(self, post, post_directory):
+        filename = post["filename"]
+        download_url = urljoin(self.POSTS_URL, post["download_uri"])
+        self.perform_download(download_url, filename)
+
+
+    def download_post_content(self, post_json, post_directory):
+        if post_json["category"] == "photo_gallery":
+            photo_gallery_title = post_json["title"]
+            photo_gallery = post_json["post_content_photos"]
+            photo_counter = 0
+            gallery_directory = os.path.join(sanitize_for_path(post_directory), sanitize_for_path(photo_gallery_title))
+            os.mkdir(gallery_directory)
+            for photo in photo_gallery:
+                self.download_photo(photo, photo_counter, gallery_directory)
+                photo_counter += 1
+        elif post_json["category"] == "file":
+            self.download_video(post_json, post_directory)
+
+
     def download_post(self, post_id):
         response = self.session.get(self.POST_API.format(post_id))
         response.raise_for_status()
-        response_json = json.loads(response.text)
-        print(json.dumps(response_json, sort_keys=True, indent=4))
+        post_json = json.loads(response.text)["post"]
+        post_id = post_json["id"]
+        print(post_id)
+        post_title = post_json["title"]
+        post_contents = post_json["post_contents"]
+        post_directory = sanitize_for_path(str(post_id) + " - " + post_title)
+        os.mkdir(post_directory)
+        for post in post_contents:
+            self.download_post_content(post, post_directory)
 
 
 class FantiaClub:
     def __init__(self, fanclub_id):
         self.id = fanclub_id
+
+
+def sanitize_for_path(value, replace=' '):
+    """Remove potentially illegal characters from a path."""
+    return re.sub(r'[<>\"\?\\\/\*:]', replace, value)
 
 
 if __name__ == "__main__":
